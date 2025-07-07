9c0e718e091b8beb41e05a0df3fa302a40fc5370
Saurab Shrestha <saurab.shrestha99@gmail.com>
2025-07-07 17:38:27 +0545

    Complete home page.

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,73 +1,23 @@
 import Image from "next/image";
 import React from "react";
+import BookCard from "./BookCard";
 
-const BookList = () => {
+interface Props {
+  title: string;
+  books: Book[];
+  containerClassName?: string;
+}
+
+const BookList = ({ title, books, containerClassName }: Props) => {
   return (
-    <div className="flex flex-col my-20">
-      <div className="text-light-100 font-semibold">Popular Books</div>
-      <div className="flex flex-row my-10 gap-10 justify-between">
-        <div className="flex flex-col gap-2">
-          <Image
-            src="/images/auth-illustration.png"
-            alt="Book"
-            width={150}
-            height={100}
-          />
-          <div className="text-light-100">Book Title</div>
-          <div className="text-light-100 italic">Genre</div>
-        </div>
-        <div className="flex flex-col gap-2">
-          <Image
-            src="/images/auth-illustration.png"
-            alt="Book"
-            width={150}
-            height={100}
-          />
-          <div className="text-light-100">Book Title</div>
-          <div className="text-light-100 italic">Genre</div>
-        </div>
-        <div className="flex flex-col gap-2">
-          <Image
-            src="/images/auth-illustration.png"
-            alt="Book"
-            width={150}
-            height={100}
-          />
-          <div className="text-light-100">Book Title</div>
-          <div className="text-light-100 italic">Genre</div>
-        </div>
-        <div className="flex flex-col gap-2">
-          <Image
-            src="/images/auth-illustration.png"
-            alt="Book"
-            width={150}
-            height={100}
-          />
-          <div className="text-light-100">Book Title</div>
-          <div className="text-light-100 italic">Genre</div>
-        </div>
-        <div className="flex flex-col gap-2">
-          <Image
-            src="/images/auth-illustration.png"
-            alt="Book"
-            width={150}
-            height={100}
-          />
-          <div className="text-light-100">Book Title</div>
-          <div className="text-light-100 italic">Genre</div>
-        </div>
-        <div className="flex flex-col gap-2">
-          <Image
-            src="/images/auth-illustration.png"
-            alt="Book"
-            width={150}
-            height={100}
-          />
-          <div className="text-light-100">Book Title</div>
-          <div className="text-light-100 italic">Genre</div>
-        </div>
-      </div>
-    </div>
+    <section className={containerClassName}>
+      <h2 className="font-bebas-neue text-4xl text-light-100">{title}</h2>
+      <ul className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
+    </section>
   );
 };
 
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,56 +1,74 @@
 import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
+import BookCover from "./BookCover";
 
-const BookOverview = () => {
+const BookOverview = ({
+  title,
+  author,
+  genre,
+  rating,
+  totalCopies,
+  availableCopies,
+  description,
+  color,
+  coverImage,
+}: Book) => {
+  console.log("cover url", coverImage);
   return (
-    <>
-      <div className="flex flex-row gap-10 text-light-100 justify-between">
-        <div className="flex flex-col gap-1 w-1/2 ">
-          <div className="text-4xl">Origin</div>
-          {/* <div>By c</div> */}
-          <div className="flex flex-row gap-5">
-            <div className="flex flex-row">
-              By <div className="text-light-200 mx-2">Dan Brown</div>
-            </div>
-            <div className="flex flex-row">
-              Category{" "}
-              <div className="text-light-200 mx-2">Thriller / Suspense</div>
-            </div>
-            <div className="flex flex-row">
-              <Image src="icons/star.svg" alt="Book" width={15} height={15} />
-              <div className="text-light-200 mx-2">4.5/5</div>
-            </div>
-          </div>
-          <div className="flex flex-row gap-5">
-            <div className="flex flex-row">
-              Total books: <div className="text-light-200 mx-2">100</div>
-            </div>
-            <div className="flex flex-row">
-              Available books: <div className="text-light-200 mx-2">42</div>
-            </div>
-          </div>
-          <br />
-          <div>
-            Very long descriptions Very long descriptions Very long descriptions
-            Very long descriptions Very long descriptions Very long descriptions
-            Very long descriptions Very long descriptions Very long descriptions
-          </div>
-          <br />
+    <section className="book-overview">
+      <div className="flex flex-1 flex-col gap-5">
+        <h1>{title}</h1>
+        <div className="book-info">
+          <p>
+            By <span className="font-semibold text-light-200">{author}</span>
+          </p>
 
-          <Button size="sm" className="w-48 text-dark-100">
-            <Image src="icons/book.svg" alt="Book" width={15} height={15} />
-            Borrow Book Request
+          <p>
+            Category{" "}
+            <span className="font-semibold text-light-200">{genre}</span>
+          </p>
+
+          <div className="flex flex-row gap-1">
+            <Image src="icons/star.svg" alt="star" width={22} height={22} />
+            <p>{rating}</p>
+          </div>
+          <div className="book-copies">
+            <p>
+              Total Books: <span>{totalCopies}</span>
+            </p>
+
+            <p>
+              Available Books: <span>{availableCopies}</span>
+            </p>
+          </div>
+
+          <p className="book-description">{description}</p>
+          <Button className="book-overview_btn">
+            <Image src="icons/book.svg" alt="Book" width={20} height={20} />
+            <p className="font-bebas-neue text-xl text-dark-100">Borrow Book</p>
           </Button>
         </div>
-        <Image
-          src="/images/auth-illustration.png"
-          alt="Book"
-          width={250}
-          height={250}
-        />
       </div>
-    </>
+      <div className="relative flex flex-1 justify-center">
+        <div className="relative">
+          <BookCover
+            variant="wide"
+            className="z-10"
+            coverColor={color}
+            coverImage={coverImage}
+          />
+
+          <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
+            <BookCover
+              variant="wide"
+              coverColor={color}
+              coverImage={coverImage}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
